826fda3335a0bb139b80fdb0bba498e4696f90aa
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2023-09-24 22:42:22 +0300

    Add scss variables, themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React, { Suspense } from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import "./index.scss";
+import { useTheme } from "./theme/useTheme";
+import "./styles/index.scss";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
 
