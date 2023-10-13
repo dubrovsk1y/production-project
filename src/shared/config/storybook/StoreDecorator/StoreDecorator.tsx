f803a3de5af8fbdfe74b837380caa1dcd976dde5
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2023-10-14 00:41:23 +0300

    Add LoginForm stories, update StoreDecorator

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,9 +1,25 @@
-import { DeepPartial } from "@reduxjs/toolkit";
 import { Story } from "@storybook/react";
+import { DeepPartial, ReducersMapObject } from "@reduxjs/toolkit";
 import { StateSchema, StoreProvider } from "app/providers/StoreProvider";
+import { loginReducer } from "features/AuthByUsername/model/slice/loginSlice";
 
-export const StoreDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: Story) => (
-  <StoreProvider initialState={state as StateSchema}>
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+  loginForm: loginReducer,
+};
+
+export const StoreDecorator = (
+  state: DeepPartial<StateSchema>,
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
+) => (StoryComponent: Story) => (
+  <StoreProvider
+    initialState={state as StateSchema}
+    asyncReducers={
+      {
+        ...defaultAsyncReducers as ReducersMapObject<StateSchema>,
+        ...asyncReducers as ReducersMapObject<StateSchema>,
+      }
+    }
+  >
     <StoryComponent />
   </StoreProvider>
 );
